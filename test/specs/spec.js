97a4f29206c08b5af90cbe1039c4782d58a9d058
Yauheni Tsiarletski <Yauheni_Tsiarletski@epam.com>
2019-02-08 16:08:06 +0300

    created

---
+++ test/specs/spec.js
@@ -0,0 +1,20 @@
+// describe('basic test', () => {
+//   console.log('\nTest starts!\n');
+//   it('should check sum', () => {
+//     const selector = 'new UiSelector().text("2").className("android.widget.TextView").instance[14]';
+//     browser.click('android=' + selector);
+    // $('#kb_2').click();
+    // $('#kb_plus').click();
+    // $('#kb_3').click();
+    // browser.element('#kb_equal').click();
+    // console.log(`${browser.element('#text').getText()}`);
+    // expect(browser.element('#text').getText()).toEqual('5', 'Incorrect result');
+//   });
+// });
+
+describe ('basic test', () => {
+
+  it('should open the app', () => {
+    console.log('\nIt works!!!\n');
+  });
+});
